e30dfbfd1c825de35b9eb625d2b98b3e15804273
achmad.maulana <achmad.maulana.and@gmail.com>
2018-08-08 23:30:04 +0700

    update 20180807

--- assets/js/animated-sticky-header.js
+++ assets/js/animated-sticky-header.js
@@ -1,8 +1,10 @@
 $(window).scroll(function() {
     if ($(this).scrollTop() > 1){  
         $('header').addClass("sticky");
+        $('.text').addClass("sticky");
     }
     else{
         $('header').removeClass("sticky");
+        $('.text').removeClass("sticky");
     }
 });
