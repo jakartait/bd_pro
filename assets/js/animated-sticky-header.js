96503bcae3ded8fd15bee9a7e021cb91a21660ab
mnbv <achmad.maulana@big-solution.net>
2018-08-09 16:36:02 +0700

    commit on github

--- assets/js/animated-sticky-header.js
+++ assets/js/animated-sticky-header.js
@@ -2,10 +2,8 @@ $(window).scroll(function () {
     if ($(this).scrollTop() > 1) {
         $('header').addClass("sticky");
         $('.text').addClass("sticky");
-        $('.login').show();
-    } else {
+     } else {
         $('header').removeClass("sticky");
         $('.text').removeClass("sticky");
-        $('.login').hide();
-    }
+     }
 });
